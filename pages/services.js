2d83363eace0cc551f148dbfbece564098fd60d4
benjamin1160 <benjamin@mobilehomeguys.ai>
2025-06-22 21:31:20 -0400

    Update services.js

--- pages/services.js
+++ pages/services.js
@@ -1,1 +1,21 @@
+import Head from 'next/head';
+
+export default function Services() {
+  return (
+    <>
+      <Head>
+        <title>Dog Grooming Services - Pawradise Estero</title>
+      </Head>
+      <main>
+        <h1>Our Services</h1>
+        <ul>
+          <li>Unlimited Bathing Membership - $49/mo</li>
+          <li>Tidy Trims</li>
+          <li>Nail Clipping</li>
+          <li>Teeth Brushing</li>
+        </ul>
+      </main>
+    </>
+  );
+}
 
