eb8d60d0f11748defda7f1a6a21833e75458b1e0
benjamin1160 <benjamin@mobilehomeguys.ai>
2025-06-22 21:32:47 -0400

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,1 +1,16 @@
+import Head from 'next/head';
+
+export default function Blog() {
+  return (
+    <>
+      <Head>
+        <title>Blog - Pawradise Estero</title>
+      </Head>
+      <main>
+        <h1>Blog</h1>
+        <p>Coming soon: grooming tips, pet care guides, and more.</p>
+      </main>
+    </>
+  );
+}
 
