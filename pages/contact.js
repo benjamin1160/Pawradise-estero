675feb875bc606c09c53601948a6a44d58782b7e
benjamin1160 <benjamin@mobilehomeguys.ai>
2025-06-22 21:31:40 -0400

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,1 +1,18 @@
+import Head from 'next/head';
+
+export default function Contact() {
+  return (
+    <>
+      <Head>
+        <title>Contact - Pawradise Estero</title>
+      </Head>
+      <main>
+        <h1>Contact Us</h1>
+        <p>Email: hello@pawradise.com</p>
+        <p>Phone: (239) 555‑DOGS</p>
+        <p>Location: Estero, FL</p>
+      </main>
+    </>
+  );
+}
 
