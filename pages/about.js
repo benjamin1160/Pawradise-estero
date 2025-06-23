e7687f190b48298c454a27ae6ae346e2e6b583c5
benjamin1160 <benjamin@mobilehomeguys.ai>
2025-06-22 21:30:36 -0400

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,16 @@
 
+import Head from 'next/head';
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About - Pawradise Estero</title>
+      </Head>
+      <main>
+        <h1>About Us</h1>
+        <p>Pawradise Estero has been pampering pets since 2009. We treat every dog like family.</p>
+      </main>
+    </>
+  );
+}
